317f1cc7d9b774d33713f6ce66e4a5795b6ac992
busraceylaann <busraceylaann@users.noreply.github.com>
2021-07-08 21:32:50 +0300

    aciklama satırları eklendi

--- client/Client.py
+++ client/Client.py
@@ -1,32 +1,31 @@
 import socket
-import io
 import signal, sys, time
 import argparse
 
-
+#Docker işlemi yapacağımız için,serverin hostunu burada parse ediyoruz.Bunun için argparse modülü kullanıyoruz.
+#ArgumentParser üzerinden yeni bir nesne oluşturuyoruz.
 parser = argparse.ArgumentParser(description='calculation descrription')
+# gerekli argümanları ekliyoruz
 parser.add_argument('--host', action="store", dest="host", required=True, help="joblist as dict")
 args = parser.parse_args()
+
 server = socket.socket()
 
-
+#Ctrl-C ile programı sonlandırmak istiyorum,sinyal  oluşturuyorum.
 def signal_handler(signal, frame):
     print ('Ctrl+C ')
     server.close()
     time.sleep(1)
     sys.exit(0)
-
-
 signal.signal(signal.SIGINT, signal_handler)
-
 
 try:
     server.connect((args.host, 4444))
     while True:
-        string = input("Problem Girin:")
-        server.send(string.encode())
-        result = server.recv(1024).decode()
-        print("Cevap:", result)
+        string = input("Problem Girin:") #kullanıcıdan problemi alıyoruz
+        server.send(string.encode()) #servera problemı gönderiyoruz
+        result = server.recv(1024).decode() #serverdan gelen cevabı alıp result değişkenine atıyoruz
+        print("Cevap:", result) #cevabı yazdırıyoruz
 
 except(Exception):
     server.send("Hata".encode())
--- server/Server.py
+++ server/Server.py
@@ -1,25 +1,23 @@
 import socket
 
 host = "0.0.0.0"  #socket.gethostname()
-port = 4444
-server = socket.socket()
-server.bind((host, port))
-# why u r waiting five connection
-server.listen(5)
+port = 4444 #4444 portuna baglanmasını istiyorum
+server = socket.socket()  #soket nesnesi olusturdum
+server.bind((host, port)) #verdiğimiz host ve ip adreslerine bağlan
+server.listen(5) #sunucunun aynı anda 5 istemciyi dinlemesini istedim
 print("Server Çalışıyor")
 
 
 while(True):
-    client, address = server.accept()
+    client, address = server.accept() #istemci ve adresi kabul edilir
     print('Bağlanan Adres', address)
     while(True):
          try:
-              problem=client.recv(1024).decode()
-              print("Problem:", problem)
-              result=eval(problem)
-              client.send(str(result).encode())
+              problem=client.recv(1024).decode()#istemciden gelen,ara bellek boyutu 1024 olan byte tipindeki mesaj stringe dönüşür
+              print("Problem:", problem) #istemciden gelen mesaj
+              result=eval(problem) #eval fonksiyonu ile matematiksel işlemi yaptıktan sonra result değişkenıne atanır
+              client.send(str(result).encode())   # İstemciye gelen mesajı yanıt olarak geri gönderiyoruz.
          except (Exception ):
                client.send("Hata".encode())
 
-
-    client.close()
+    client.close() #istemciyi sonlandırıyoruz.
